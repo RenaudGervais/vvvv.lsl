18b34ea4400917be067e29fbc80139f446eab7b3
Renaud Gervais <renaud.gervais@gmail.com>
2016-02-01 14:38:43 +0000

    Dynamic pin working

--- src/Nodes/LSLSendDataNode.cs
+++ src/Nodes/LSLSendDataNode.cs
@@ -93,10 +93,10 @@ namespace VVVV.Nodes
 
         private void HandleNbStreamChanged(IDiffSpread<string> sender)
         {
-            Spread<int> nbSlice = new Spread<int>(FResourceName.SliceCount);
+            Spread<int> nbSlice = new Spread<int>(new int[] { FResourceName.SliceCount });
 
             //Create the pins for data
-            HandlePinCountChanged(nbSlice, FData, (i) => new InputAttribute("Data " + FResourceName[i]));
+            HandlePinCountChanged(nbSlice, FData, (i) => new InputAttribute("Data " + FResourceName[i].ToString()));
         }
         #endregion pin management
 
@@ -107,82 +107,11 @@ namespace VVVV.Nodes
 
         void InitializeStream(string type, string[] name)
         {
-            //Create the stream info and outlet
-            mInfo = new liblsl.StreamInfo()
-
-            // wait until an EEG stream shows up
-            results = liblsl.resolve_stream("type", FResourceType[0], 1, 1);
-
-            Flogger.Log(LogType.Debug, "Number of streams: " + results.Length);
-
-            for (int i = 0; i < results.Length; i++)
-            {
-                liblsl.StreamInlet tmpInlet = new liblsl.StreamInlet(results[i], FMaxBufLen[0]);
-                liblsl.StreamInfo tmpInfo = tmpInlet.info();
-                Flogger.Log(LogType.Debug, "Look at stream name: " + tmpInfo.name());
-                if (FResourceName[0].Equals(tmpInfo.name()))
-                {
-                    Flogger.Log(LogType.Debug, "Bingo!");
-                    // retrieve data
-                    info = tmpInfo;
-                    inlet = tmpInlet;
-                    FNBChannels[0] = info.channel_count();
-                    FSampleRate[0] = info.nominal_srate();
-                    Flogger.Log(LogType.Debug, "Nb channels: " + info.channel_count());
-                    Flogger.Log(LogType.Debug, "Sample rate: " + info.nominal_srate());
-                    break;
-                }
-            }
         }
 		
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
-            // Try to establish connexion when input stream name changed or if
-            // manually triggered
-            if (FResourceName.IsChanged || FResourceType.IsChanged || FFindStream[0])
-                Connect();
-
-            if (FNBChannels[0] > 0)
-            {
-				// First slices: channels
-        		FOutput.SliceCount = FNBChannels[0];
-				
-				// VPRN tells us how many values we have and we know the size of a chunk: easy to compute how many channels we receive
-       			//FNBChannels[0] = 1;// e.Channels.Length / FChunkSize[0];
-        	
-				// pull all we can
-				int totalChunks = 0;
-				float[,] sample = new float[FChunkSize[0],FNBChannels[0]];
-				double[] timestamps = new double[FChunkSize[0]];
-				int nbChunks = -1;
-				//Flogger.Log(LogType.Debug,"new loop ");
-				while(nbChunks != 0 && totalChunks < FMaxSamples[0] ) {
-					nbChunks = inlet.pull_chunk( sample, timestamps, FTimeOut[0]);
-					//Flogger.Log(LogType.Debug,"timestamp: " + nbChunks);
-					totalChunks += nbChunks;
-					
-					// try to fill
-					for (int chan = 0; chan < FNBChannels[0]; chan++) {
-						// Within slices we have chunks
-						FOutput[chan].SliceCount = totalChunks;
-						for (int i = 0; i < nbChunks; i++) {
-							// fill from the end
-							FOutput[chan][totalChunks-nbChunks+i] = sample[i,chan];
-						}
-					}	
-				}
-				
-            	//for (int chan = 0; chan < FNBChannels[0]; chan++) {
-            	//	// Within slices we have chunks
-				//	FOutput[chan].SliceCount = FChunkSize;
-            	//	for (int i = 0; i < FChunkSize; i++) {
-            	//		// We move to the correct position
-            	//		int pos = chan * FChunkSize + i;
-            	//		FOutput[chan][i] = 1;//e.Channels[pos];
-            	//	}
-		   		//}
-			}
 		}
 	}
 }
